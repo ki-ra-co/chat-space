38740e0da00a28c6d57fe27851715fad15f1981b
KI-RA-CO <kihara4173@icloud.com>
2020-01-22 12:33:41 +0900

    group-management-add_groups-finish
    作成したグループの保存機能の実装終了

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,9 @@
 class GroupsController < ApplicationController
 
+  def index
+    
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -10,16 +14,21 @@ class GroupsController < ApplicationController
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
-      render :@new
+      render :new
     end
   end
 
   def edit
-
+    @group = Group.find(params[:id])
   end
 
   def update
-
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path,notice: 'グループを更新しました'
+    else
+      render :edit
+    end
   end
   
 
