7261960e476b0f8ec639a84196c875a2d8be3d7a
KI-RA-CO <kihara4173@icloud.com>
2020-01-26 20:08:05 +0900

    完成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,65 +1,66 @@
 $(function(){ 
-  function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="chat__main__message-list__top" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="chat__main__message-list__top_name">
-             ${message.user_name}
-           </div>
-           <div class="chat__main__message-list__top_daytime">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="chat__main__message-list__bottom">
-           <p class="chat__main__message-list__bottom__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="chat__main__message-list__top" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="chat__main__message-list__top_name">
-             ${message.user_name}
-           </div>
-           <div class="chat__main__message-list__top_daytime">
-             ${message.created_at}
-           </div>
-         </div>
-         <div class="chat__main__message-list__bottom">
-           <p class="chat__main__message-list__bottom__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+    function buildHTML(message){
+      if ( message.image ) {
+        var html =
+          `<div class="chat__main__message-list__top" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="chat__main__message-list__top_name">
+                ${message.user_name}
+              </div>
+              <div class="chat__main__message-list__top_daytime">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="chat__main__message-list__bottom">
+              <p class="chat__main__message-list__bottom__content">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+          `<div class="chat__main__message-list__top" data-message-id=${message.id}>
+            <div class="upper-message">
+              <div class="chat__main__message-list__top_name">
+                ${message.user_name}
+              </div>
+              <div class="chat__main__message-list__top_daytime">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="chat__main__message-list__bottom">
+              <p class="chat__main__message-list__bottom__content">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
 $('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.message').append(html);
-    $('form')[0].reset();
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    $('chat__main__message-form__container__send').removeAttr('data-disable-with')
-  })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
-  });
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat__main__message-list').append(html);
+      $('form')[0].reset();
+      $('.chat__main__message-list').animate({ scrollTop: $('.chat__main__message-list')[0].scrollHeight});
+      $('.chat__main__message-form__container__send').removeAttr('disabled',false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
 })
 });
+
