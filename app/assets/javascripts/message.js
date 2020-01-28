8d6e758372271f11013215f864a798f0b99d2af9
KI-RA-CO <kihara4173@icloud.com>
2020-01-28 21:05:56 +0900

    自動更新実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,49 @@
 $(function(){ 
-    function buildHTML(message){
-      if ( message.image ) {
+   var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.chat__main__message-list__container:last').data("message-id");
+    group_id = $('.chat__main__message-list').data("group-id");
+
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: `/groups/${group_id}/api/messages`,
+
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log(messages)
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat__main__message-list').append(insertHTML);
+        $('.chat__main__message-list').animate({ scrollTop: $('.chat__main__message-list')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+
+  function buildHTML(message){
+      if ( message.content && message.image ) {
         var html =
-          `<div class="chat__main__message-list__top" data-message-id=${message.id}>
+        `<div class="chat__main__message-list__container" data-message-id=${message.id}>
+          <div class="chat__main__message-list__top" >
             <div class="upper-message">
               <div class="chat__main__message-list__top_name">
                 ${message.user_name}
@@ -19,9 +60,10 @@ $(function(){
             <img src=${message.image} >
           </div>`
         return html;
-      } else {
+      } else if (message.content) {
         var html =
-          `<div class="chat__main__message-list__top" data-message-id=${message.id}>
+        `<div class="chat__main__message-list__container" data-message-id=${message.id}>
+          <div class="chat__main__message-list__top" >
             <div class="upper-message">
               <div class="chat__main__message-list__top_name">
                 ${message.user_name}
@@ -37,8 +79,27 @@ $(function(){
             </div>
           </div>`
         return html;
-      };
-    }
+      } else if (message.image) {
+        var html =
+        `<div class="chat__main__message-list__container" data-message-id=${message.id}>  
+          <div class="chat__main__message-list__top" >
+            <div class="upper-message">
+              <div class="chat__main__message-list__top_name">
+                ${message.user_name}
+              </div>
+              <div class="chat__main__message-list__top_daytime">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="chat__main__message-list__bottom">
+              <p class="chat__main__message-list__bottom__content">
+                ${message.image}
+              </p>
+            </div>
+          </div>`
+        return html;  
+      }
+  };
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -53,6 +114,7 @@ $('#new_message').on('submit', function(e){
     })
     .done(function(data){
       var html = buildHTML(data);
+      console.log(html)
       $('.chat__main__message-list').append(html);
       $('form')[0].reset();
       $('.chat__main__message-list').animate({ scrollTop: $('.chat__main__message-list')[0].scrollHeight});
